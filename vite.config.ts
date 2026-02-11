4bd681e611d6b6b6efcdc6f20869e6a5d19052be
FuckCray <51244165+getrttr@users.noreply.github.com>
2026-02-11 22:35:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/probable-meme/', 
-      
+      base: '/probable-meme/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -16,6 +15,6 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         },
       },
-      plugins: [react()], 
+      plugins: [react()],
     };
 });
